75f5a9af8695d9fc0e237045c28e3f3d560504f2
Paul Hagedorn <panzer1119@gmail.com>
2024-06-22 13:35:07 +0200

    feat: regenerate prepare_cloud_image.sh by prompt
    add an option to directly specify an image name (that is then taken from the specified or default storage if present), so we do not need to download anything
    
    add an option to specify a sha256 hash (default is not set), and if given, use that to compare it to the pre-existing image locally (and do not download the sha256 sums from the internet)

--- prepare_cloud_image.sh
+++ prepare_cloud_image.sh
@@ -17,6 +17,8 @@ usage() {
   echo "  -s, --storage   Proxmox storage ID (default: tn-core-1)"
   echo "  -f, --force     Force re-download of the image"
   echo "  --suffix        Custom image name suffix (default: -custom-docker)"
+  echo "  --image-name    Specify an image name to use from storage"
+  echo "  --sha256        Specify a SHA256 hash for local image verification"
   echo "  -h, --help      Display this help message"
   exit 1
 }
@@ -64,6 +66,8 @@ main() {
   local storage="tn-core-1"
   local force_download=false
   local custom_suffix="-custom-docker"
+  local image_name=""
+  local sha256_hash=""
 
   # Parse options
   while [[ $# -gt 0 ]]; do
@@ -100,6 +104,14 @@ main() {
         custom_suffix="$2"
         shift 2
         ;;
+      --image-name)
+        image_name="$2"
+        shift 2
+        ;;
+      --sha256)
+        sha256_hash="$2"
+        shift 2
+        ;;
       -h|--help)
         usage
         ;;
@@ -117,61 +129,71 @@ main() {
   local storage_path
   storage_path=$(check_proxmox_storage "${storage}")
 
-  # Determine the download URL and checksum URL based on options
-  local img_url checksum_url checksum_file img_name img_path temp_img custom_img_name custom_img_path expected_checksum existing_checksum
+  # Determine the download URL and checksum URL based on options if image name is not specified
+  local img_url checksum_url checksum_file img_path temp_img custom_img_name custom_img_path expected_checksum existing_checksum
 
-  case ${distro} in
-    ubuntu)
-      img_url="https://cloud-images.ubuntu.com/${release}/current/${release}-server-cloudimg-${arch}.img"
-      checksum_url="https://cloud-images.ubuntu.com/${release}/current/SHA256SUMS"
-      checksum_file="SHA256SUMS"
-      ;;
-    debian)
-      img_url="https://cloud.debian.org/images/cloud/OpenStack/current-${release}/debian-${release}-openstack-${arch}.qcow2"
-      checksum_url="https://cloud.debian.org/images/cloud/OpenStack/current-${release}/SHA256SUMS"
-      checksum_file="SHA256SUMS"
-      ;;
-    fedora)
-      img_url="https://download.fedoraproject.org/pub/fedora/linux/releases/${version}/Cloud/${arch}/images/Fedora-Cloud-Base-${version}.${arch}.qcow2"
-      checksum_url="https://download.fedoraproject.org/pub/fedora/linux/releases/${version}/Cloud/${arch}/images/CHECKSUM"
-      checksum_file="CHECKSUM"
-      ;;
-    *)
-      echo "Unsupported distribution: ${distro}" >&2
+  if [ -n "${image_name}" ]; then
+    img_path="${storage_path}/${image_name}"
+    if [ ! -f "${img_path}" ]; then
+      echo "Error: Specified image ${img_path} does not exist in storage."
       exit 1
-      ;;
-  esac
-
-  # Download the checksum file
-  wget -O "${checksum_file}" "${checksum_url}"
-
-  # Extract the checksum for the image
-  expected_checksum=$(grep $(basename ${img_url}) ${checksum_file} | awk '{ print $1 }')
-
-  # Download the image if not already present or if checksum does not match or force download
-  img_name="${distro}-${release}-${version}-${arch}.img"
-  img_path="${storage_path}/${img_name}"
-
-  if [ "${force_download}" = true ]; then
-    echo "Forcing download of the image."
-    wget -O "${img_path}" "${img_url}"
+    fi
   else
-    if [ -f "${img_path}" ]; then
-      echo "Image already exists in storage. Checking hash..."
-      existing_checksum=$(sha256sum "${img_path}" | awk '{ print $1 }')
-      if [ "${existing_checksum}" == "${expected_checksum}" ]; then
-        echo "Image checksum matches. Using existing image."
+    case ${distro} in
+      ubuntu)
+        img_url="https://cloud-images.ubuntu.com/${release}/current/${release}-server-cloudimg-${arch}.img"
+        checksum_url="https://cloud-images.ubuntu.com/${release}/current/SHA256SUMS"
+        checksum_file="SHA256SUMS"
+        ;;
+      debian)
+        img_url="https://cloud.debian.org/images/cloud/OpenStack/current-${release}/debian-${release}-openstack-${arch}.qcow2"
+        checksum_url="https://cloud.debian.org/images/cloud/OpenStack/current-${release}/SHA256SUMS"
+        checksum_file="SHA256SUMS"
+        ;;
+      fedora)
+        img_url="https://download.fedoraproject.org/pub/fedora/linux/releases/${version}/Cloud/${arch}/images/Fedora-Cloud-Base-${version}.${arch}.qcow2"
+        checksum_url="https://download.fedoraproject.org/pub/fedora/linux/releases/${version}/Cloud/${arch}/images/CHECKSUM"
+        checksum_file="CHECKSUM"
+        ;;
+      *)
+        echo "Unsupported distribution: ${distro}" >&2
+        exit 1
+        ;;
+    esac
+
+    # Download the checksum file if SHA256 hash is not provided
+    if [ -z "${sha256_hash}" ]; then
+      wget -O "${checksum_file}" "${checksum_url}"
+      expected_checksum=$(grep $(basename ${img_url}) ${checksum_file} | awk '{ print $1 }')
+    else
+      expected_checksum="${sha256_hash}"
+    fi
+
+    # Download the image if not already present or if checksum does not match or force download
+    local img_name="${distro}-${release}-${version}-${arch}.img"
+    img_path="${storage_path}/${img_name}"
+
+    if [ "${force_download}" = true ]; then
+      echo "Forcing download of the image."
+      wget -O "${img_path}" "${img_url}"
+    else
+      if [ -f "${img_path}" ]; then
+        echo "Image already exists in storage. Checking hash..."
+        existing_checksum=$(sha256sum "${img_path}" | awk '{ print $1 }')
+        if [ "${existing_checksum}" == "${expected_checksum}" ]; then
+          echo "Image checksum matches. Using existing image."
+        else
+          echo "Image checksum does not match. Downloading new image."
+          wget -O "${img_path}" "${img_url}"
+        fi
       else
-        echo "Image checksum does not match. Downloading new image."
         wget -O "${img_path}" "${img_url}"
       fi
-    else
-      wget -O "${img_path}" "${img_url}"
     fi
   fi
 
   # Copy image to temporary folder for customization
-  temp_img="/tmp/${img_name}"
+  temp_img="/tmp/$(basename ${img_path})"
   cp "${img_path}" "${temp_img}"
 
   # Customize the image with virt-customize
@@ -183,7 +205,7 @@ main() {
     --run-command 'rm -f get-docker.sh'
 
   # Rename and move the customized image back to the storage location
-  custom_img_name="${distro}-${release}-${version}-${arch}${custom_suffix}.img"
+  custom_img_name="$(basename ${img_path} .img)${custom_suffix}.img"
   custom_img_path="${storage_path}/${custom_img_name}"
   mv "${temp_img}" "${custom_img_path}"
 
